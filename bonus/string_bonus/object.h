761bc503dd8bbb5395c50a3844b532b7f221ddd2
collin_b <collin_b@epitech.eu>
2014-01-11 22:18:11 +0100

    add buggy fucking shit

--- bonus/string_bonus/object.h
+++ bonus/string_bonus/object.h
@@ -10,6 +10,8 @@ typedef void (*ctor_t)(Object * self, va_list * args);
 typedef void (*dtor_t)(Object * self);
 typedef char const* (*to_string_t)(Object* self);
 typedef char const* (*get_string)(Object* self);
+typedef Object* (*append_string_c)(Object* self, Object* other);
+typedef Object* (*append_string_s)(Object* self, char const * other);
 
 //typedef Object* (*binary_operator_t)(const Object* self, const Object* other);
 
@@ -20,10 +22,14 @@ typedef struct {
   dtor_t		__del__;
   to_string_t		__str__;
   get_string		__getstr__;
+  append_string_c	__apstrc__;
+  append_string_s	__apstrs__;
   //    binary_operator_t   __add__;
 } Class;
 
 # define str(o) (((Class*) o)->__str__ != NULL ? ((Class*) o)->__str__(o) : "")
 # define getstr(o) (((Class*) o)->__getstr__ != NULL ? ((Class*) o)->__getstr__(o) : "")
+# define appstr_c(o, p) (((Class*) o)->__apstrc__ != NULL ? ((Class*) o)->__apstrc__(o) : "")
+# define appstr_s(o, p) (((Class*) o)->__apstrs__ != NULL ? ((Class*) o)->__apstrs__(o) : "")
 //# define add(a, b) (((Class*) a)->__add__(a, b))
 #endif
