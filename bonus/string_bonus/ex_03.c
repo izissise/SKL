37dee55a00bdf9145aa1f8e858758ba91116d37c
debas_e <debas_e@epitech.eu>
2014-01-11 19:31:31 +0100

    add class string bonus

---
+++ bonus/string_bonus/ex_03.c
@@ -0,0 +1,17 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "new.h"
+#include "string.h"
+
+int main()
+{
+  Object * p1 = new(String, "bonjour les amis");
+  
+  printf("%s\n", str(p1));
+
+  delete(p1);
+  
+  return 0;
+}
