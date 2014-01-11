93bf096002d7676858b40836d6ba3a893472696f
debas_e <debas_e@epitech.eu>
2014-01-11 09:29:23 +0100

    add new file

---
+++ ex_01/new.c
@@ -0,0 +1,10 @@
+/*
+** new.c for new in /home/debas_e/Project/piscine_cpp/Rush/piscinecpprush1/ex_01
+** 
+** Made by Etienne
+** Login   <debas_e@epitech.net>
+** 
+** Started on  Sat Jan 11 09:28:41 2014 Etienne
+** Last update Sat Jan 11 09:28:49 2014 Etienne
+*/
+
