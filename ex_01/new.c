4982628e548f78ca61d3807ff1284f82a6d1ffde
debas_e <debas_e@epitech.eu>
2014-01-11 09:50:41 +0100

    update new.c

--- ex_01/new.c
+++ ex_01/new.c
@@ -5,7 +5,7 @@
 ** Login   <debas_e@epitech.net>
 ** 
 ** Started on  Sat Jan 11 09:28:41 2014 Etienne
-** Last update Sat Jan 11 09:39:41 2014 Etienne
+** Last update Sat Jan 11 09:50:25 2014 Etienne
 */
 
 #include "object.h"
@@ -15,5 +15,7 @@ Object		*new(Class *object) {
   Object	*ret = NULL;
 
   ret = malloc(object->__size__);
-  
+  memcpy(ret, object, object->__size__);
+  ret->base->__init__(ret);
+  return (ret);
 }
