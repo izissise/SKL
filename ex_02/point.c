22a489b3fbbb2f1f039a0b0d8d0e1757c2faf9b7
lavand_m <lavand_m@epitech.eu>
2014-01-11 11:32:03 +0100

    add vararg point.c vertex.c

--- ex_02/point.c
+++ ex_02/point.c
@@ -5,42 +5,46 @@
 ** Login   <lavand_m@epitech.net>
 **
 ** Started on  Sat Jan 11 10:57:16 2014 Maxime
-** Last update Sat Jan 11 10:57:25 2014 Maxime
+** Last update Sat Jan 11 11:31:40 2014 Maxime
 */
 
+#include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "point.h"
 
 typedef struct
 {
-    Class base;
-    int x, y;
-    char *str
+  Class base;
+  int x, y;
+  char *str;
 } PointClass;
 
-static void Point_ctor(Object* self)
+static void Point_ctor(Object* self, va_list *ap)
 {
-    self->str = NULL
+
+  self->x = va_arg(*ap, int);
+  self->y = va_arg(*ap, int);
+  self->str = NULL;
 }
 
 static void Point_dtor(Object* self)
 {
-    free(self->str);
+  free(self->str);
 }
 
 static char const *Point_str(Object* self)
 {
-    if (self->str != NULL)
-        free(str);
-    self->str = malloc(33);
-    snprintf(self->str, 33, "<Point (%d, %d)>", self->x, self->y);
-    return(self->str);
+  if (self->str != NULL)
+    free(str);
+  self->str = malloc(33);
+  snprintf(self->str, 33, "<Point (%d, %d)>", self->x, self->y);
+  return(self->str);
 }
 
 static PointClass _description = {
-    { sizeof(PointClass), "Point", &Point_ctor, &Point_dtor, &Point_str },
-    0, 0
+  { sizeof(PointClass), "Point", &Point_ctor, &Point_dtor, &Point_str },
+  0, 0, NULL
 };
 
 Class* Point = (Class*) &_description;
--- ex_02/vertex.c
+++ ex_02/vertex.c
@@ -5,7 +5,7 @@
 ** Login   <lavand_m@epitech.net>
 **
 ** Started on  Sat Jan 11 10:57:39 2014 Maxime
-** Last update Sat Jan 11 10:57:48 2014 Maxime
+** Last update Sat Jan 11 11:31:29 2014 Maxime
 */
 
 #include <stdlib.h>
@@ -14,33 +14,36 @@
 
 typedef struct
 {
-    Class base;
-    int x, y, z;
-    char *str;
+  Class base;
+  int x, y, z;
+  char *str;
 } VertexClass;
 
-static void Vertex_ctor(Object* self)
+static void Vertex_ctor(Object* self, va_list *ap)
 {
-    self->str = NULL;
+  self->x = va_arg(*ap, int);
+  self->y = va_arg(*ap, int);
+  self->z = va_arg(*ap, int);
+  self->str = NULL;
 }
 
 static void Vertex_dtor(Object* self)
 {
-    free(self->str);
+  free(self->str);
 }
 
 static char const *Vertex_str(Object* self)
 {
-    if (self->str != NULL)
-        free(str);
-    self->str = malloc(46);
-    snprintf(self->str, 46, "<Vertex (%d, %d, %d)>", self->x, self->y, self->y);
-    return(self->str);
+  if (self->str != NULL)
+    free(str);
+  self->str = malloc(46);
+  snprintf(self->str, 46, "<Vertex (%d, %d, %d)>", self->x, self->y, self->y);
+  return(self->str);
 }
 
 static VertexClass _description = {
-    { sizeof(VertexClass), "Vertex", &Vertex_ctor, &Vertex_dtor, &Vertex_str },
-    0, 0, 0
+  { sizeof(VertexClass), "Vertex", &Vertex_ctor, &Vertex_dtor, &Vertex_str },
+  0, 0, 0, NULL
 };
 
 Class* Vertex = (Class*) &_description;
